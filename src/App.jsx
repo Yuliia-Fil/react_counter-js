1be0df11536d5736f40ac7ccd0412c1460d0a38d
Yulia Fil <filyulia.yf@gmail.com>
2025-05-22 21:04:32 +0300

    sol

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,12 @@ export const App = () => {
     setCount(curCount => curCount + 100);
   };
 
-  // DON'T change the code below
   const increase = () => {
     if (count % 5 === 0) {
       add100();
+    } else {
+      addOne();
     }
-
-    addOne();
   };
 
   return (
